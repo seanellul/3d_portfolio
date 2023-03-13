55e079491991eeaca610dfbc7a8ba3204da04eff
seanellul <102472328+seanellul@users.noreply.github.com>
2023-03-14 01:25:37 +0400

    First Push

---
+++ src/components/Works.jsx
@@ -0,0 +1,38 @@
+import Tilt from 'react-tilt'
+import {motion} from 'framer-motion'
+import { styles } from '../styles'
+import { github } from '../assets'
+import { SectionWrapper } from '../hoc'
+import { projects } from '../constants'
+import { fadeIn, textVariant } from '../utils/motion'
+
+
+const Works = () => {
+  return (
+
+      <>
+      
+      <motion.div variants={textVariant()}>
+      <p className={styles.sectionSubText}>My Work</p>
+      <h2 className={styles.sectionHeadText}>Projects.</h2>
+    </motion.div>
+      
+      <div
+      className='w-full flex'
+      >
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          Basically a space to talk about projects... for now.
+        </motion.p>
+
+
+      </div>
+      
+      </>
+
+    )
+}
+
+export default SectionWrapper(Works, "")
