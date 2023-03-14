6707e95877b05a849f97b6f9597935e3940d2fb8
seanellul <102472328+seanellul@users.noreply.github.com>
2023-03-14 04:19:49 +0400

    final

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,6 +6,7 @@ import { EarthCanvas } from "./canvas"
 import { SectionWrapper } from "../hoc"
 import { slideIn } from "../utils/motion"
 
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -16,9 +17,38 @@ const Contact = () => {
 
   const[loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setForm({...form,[name]: value})
 
-  const handleSubmit = (e) => {}
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs.send('service_ptp6his', 'template_xpzw3qd',
+    {
+      from_name: form.name,
+      to_name:'Sean',
+      from_email: form.email,
+      to_email: 'sean@metaverse-architects.com'
+    }, 'aDgGkZNEm-Wu90cnb').then(()=>{
+      setLoading(false);
+      alert('Thank you! I will get back to you as soon as possible.');
+
+      setForm({
+        name:'',
+        email: ''
+      })
+    }, (error) => {
+      setLoading(false)
+      console.log(error);
+
+      alert('Something went wrong.')
+    })
+
+  }
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
